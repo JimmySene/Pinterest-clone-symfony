c9e273803dbf16c9ecebe176b48da6124e7f0687
J.S <jimmy.sene@laposte.net>
2020-09-26 17:08:14 +0200

    Show all pins on index page

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -4,14 +4,17 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Pin;
+use App\Repository\PinRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class PinsController extends AbstractController
 {
     /**
      * @Route("/")
      */
-    public function index()
+    public function index(PinRepository $repo)
     {
-        return $this->render('pins/index.html.twig');
+        return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
     }
 }

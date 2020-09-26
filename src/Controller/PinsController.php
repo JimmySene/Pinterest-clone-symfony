191064f5a13d1fe600a43417a35de4b8a92cb044
J.S <jimmy.sene@laposte.net>
2020-09-26 17:42:31 +0200

    Adds creating Pin feature with html form

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -7,6 +7,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Pin;
 use App\Repository\PinRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 
 class PinsController extends AbstractController
 {
@@ -16,5 +17,25 @@ class PinsController extends AbstractController
     public function index(PinRepository $repo)
     {
         return $this->render('pins/index.html.twig', ['pins' => $repo->findAll()]);
+    }
+
+    /**
+     * @Route("/pins/create")
+     */
+    public function create(Request $request, EntityManagerInterface $em)
+    {
+        if ($request->isMethod('POST')) {
+            $data = $request->request->all();
+            $pin = new Pin();
+            $pin->setTitle($data['title']);
+            $pin->setDescription(($data['description']));
+
+            $em->persist($pin);
+            $em->flush();
+
+            return $this->redirect("/");
+        }
+        
+        return $this->render('pins/create.html.twig');
     }
 }
